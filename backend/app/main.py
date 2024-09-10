9d4e45c8e7f4199398473dd75b38eb6f5993a65f
harshsH-28 <harshsharma6419@gmail.com>
2024-09-10 21:02:44 +0530

    feat: allowed all origins for development

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,4 +8,12 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
