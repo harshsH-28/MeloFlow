3dd3bed3ca69fcea8fd195c3cf80dd21e4b355a7
harshsH-28 <harshsharma6419@gmail.com>
2024-09-17 00:02:08 +0530

    feat: add error handling in download service function

--- backend/app/services/DownloadService.py
+++ backend/app/services/DownloadService.py
@@ -1,4 +1,6 @@
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError
+from fastapi import HTTPException
 
 base_url = "https://music.youtube.com/watch?v="
 
@@ -17,6 +19,14 @@ async def download(code):
         'outtmpl': f'{download_path}/%(id)s.%(ext)s'
     }
     url = f'{base_url}{code}'
+    
+    
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except DownloadError as e:
+        print(f"Error downloading {url}: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error downloading audio: {str(e)}")
 
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+
+# /bin/ffmpeg/bin/
