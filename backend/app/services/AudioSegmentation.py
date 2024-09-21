afb2925d188999fea2097a4d86427178f09a32d1
harshsH-28 <harshsharma6419@gmail.com>
2024-09-22 02:59:46 +0530

    refactor: modularize the code and make it more readable

--- backend/app/services/AudioSegmentation.py
+++ backend/app/services/AudioSegmentation.py
@@ -1,49 +1,72 @@
+"""
+This service segments the audio into cmaf compatible segments.
+"""
+
 import os
 from fastapi import HTTPException
-import subprocess
+from app.config import FFMPEG_PATH, CMAF_CONTENT_DIR, SONGS_DIR, FFMPEG_SEGMENT_DURATION
 from app.services.DownloadService import download
+from app.utils.file_operations import ensure_directory_exists, check_file_exists
+from app.utils.ffmpeg import run_ffmpeg_command
 
-CMAF_CONTENT_DIR = "/meloflow/cmaf_files"
-
-SONGS_DIR = "/meloflow/songs"
 
 async def segment_audio(song_id: str):
+    """Segments the audio file for the given song ID."""
     input_audio_path = os.path.join(SONGS_DIR, f"{song_id}.m4a")
-
-    if not os.path.exists(input_audio_path):
-        await download(song_id)
-
     output_dir = os.path.join(CMAF_CONTENT_DIR, song_id)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir, exist_ok=True)
-        print(f"Created output directory: {output_dir}")
-    else:
-        print(f"Output directory already exists: {output_dir}")
 
-    # Segment the audio file
+    ensure_directory_exists(output_dir)
+
     output_mpd_path = os.path.join(output_dir, "manifest.mpd")
-    print(output_mpd_path)
-    command = [
-        "/meloflow/ffmpeg/bin/ffmpeg",  # Use the full path to ffmpeg
-        "-i", input_audio_path,
+
+    try:
+        await ensure_song_audio_exists(input_audio_path, song_id)
+
+        ffmpeg_command = build_ffmpeg_command(input_audio_path, output_mpd_path)
+        await run_ffmpeg_command(ffmpeg_command)
+
+        print(f"Audio processed successfully for song_id: {song_id}")
+        return {"message": "Audio processed successfully", "mpd_path": output_mpd_path, "status": True}
+
+
+    except Exception as e:
+        print(f"Error processing audio for song_id {song_id}: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error processing audio: {str(e)}") from e
+
+
+async def ensure_song_audio_exists(file_path: str, song_id: str):
+    """Ensure the audio file for the given song ID exists at the specified path, if not we download the song audio"""
+    if not check_file_exists(file_path):
+        print(f"Input file not found. Downloading song_id: {song_id}")
+        await download(song_id)
+    if not check_file_exists(file_path):
+        print(f"Input file not found after download attempt: {file_path}")
+        raise FileNotFoundError(f"Input file not found: {file_path}")
+    
+
+def build_ffmpeg_command(input_path: str, output_path: str):
+    """Builds the ffmpeg command for audio segmentation.
+
+    Args:
+        input_path (str): The path to the input audio file.
+        output_path (str): The path to save the output segmented audio file and manifest file.
+
+    Returns:
+        list: The ffmpeg command as a list of arguments.
+    """
+    return [
+        FFMPEG_PATH,  # Use the full path to ffmpeg
+        "-i", input_path,
         "-c:a", "copy",  # Copy the AAC audio without re-encoding
         "-f", "dash",
         "-use_template", "1",
         "-use_timeline", "1",
         "-init_seg_name", "init.mp4",
         "-media_seg_name", "chunk-$RepresentationID$-$Number%05d$.m4s",
-        "-seg_duration", "4",  # 4-second segments for a good balance of flexibility and efficiency
+        "-seg_duration", str(FFMPEG_SEGMENT_DURATION),
         "-adaptation_sets", "id=0,streams=a",
         "-dash_segment_type", "mp4",
-        output_mpd_path
+        "-movflags", "cmaf+dash+delay_moov",
+        "-brand", "cmfc",
+        output_path
     ]
-
-    try:
-        # Execute the ffmpeg command
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("FFmpeg Output:", result.stdout.decode())
-        print("FFmpeg Error:", result.stderr.decode())
-    except subprocess.CalledProcessError as e:
-        raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}. FFmpeg Error: {e.stderr.decode()}")
-    
-    return {"message": "Audio processed successfully", "mpd_path": output_mpd_path, "status": True}
